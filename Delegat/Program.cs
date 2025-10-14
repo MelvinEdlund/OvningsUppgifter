a9ff40e22476bedc00d2b110ca06cb769df6900b
Melvinedl <medlund01@gmail.com>
2025-10-14 14:49:02 +0200

    delegat

---
+++ Delegat/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+static string FullName(string firstName, string lastName)
+{
+    return $"{firstName} {lastName}";
+}
+
+Func<string, string, string> fullName = FullName;
+
+Console.WriteLine(FullName("sebbe", "Breuker"));
